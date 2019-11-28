6d5fd543601de25702f7184c25e53ab17638961f
Jeton Thaçi <jetonthaci@outlook.com>
2019-11-28 15:57:04 +0100

    add mode no-cors

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const fetchData = async (url) => {
   const result = await axios.get(url,
     {
+      mode: 'no-cors',
       headers: {
         'Content-Type': 'application/json',
       },
