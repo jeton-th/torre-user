e32733da0a1f917caaa8fa33c08cfe205838a76b
Jeton Thaçi <jetonthaci@outlook.com>
2019-11-28 16:52:33 +0100

    set up connections

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,23 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import fetchData from '../../utils/fetchData';
 import './Main.scss';
 
-const Main = ({ user }) => (
-  <div className="content">
-    <div className="container">
-      {user && (
-      <div className="user">
-        {user.picture && (<img src={user.picture} alt="avatar" />)}
+const Main = ({ user }) => {
+  const [connections, setConnections] = useState([]);
 
-        <div className="info">
-          <h1>{user.name}</h1>
-          <p>{user.professionalHeadline}</p>
+  useEffect(() => {
+    if (!user) return;
+
+    fetchData(`https://torre.bio/api/people/${user.publicId}/connections`)
+      .then((res) => res.data)
+      .then((data) => {
+        setConnections(data);
+      });
+  }, [user]);
+
+  return (
+    <div className="content">
+      <div className="container">
+        {user && (
+        <div className="user">
+          {user.picture && (<img src={user.picture} alt="avatar" />)}
+
+          <div className="info">
+            <h1>{user.name}</h1>
+            <p>{user.professionalHeadline}</p>
+          </div>
         </div>
+        )}
       </div>
-      )}
+
+      <div className="connections">
+        {
+          connections.length > 0 && (
+            <div>
+              <h2>Connections</h2>
+              <ul>
+                {
+                connections.map((c) => (
+                  <li key={c.person.id}>{c.person.name}</li>
+                ))
+                }
+              </ul>
+            </div>
+          )
+        }
+      </div>
     </div>
-  </div>
-);
+  );
+};
 Main.propTypes = {
   user: PropTypes.object,
 };
