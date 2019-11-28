7a5a444490de9fda66e43410e43f1a1b2d13fa8b
Jeton Thaçi <jetonthaci@outlook.com>
2019-11-28 12:37:29 +0100

    add autocomplete to search

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import PropType from 'prop-types';
 
-const Header = ({ searchText, searchHandler }) => (
+const Header = ({
+  searchText, searchHandler, searchList, userSelect,
+}) => (
   <header className="main-header">
-    <form>
-      <input type="search" onChange={searchHandler} value={searchText} placeholder="Search a user ..." autoFocus="autofocus" />
-      {/* <input type="submit" onSubmit={submitSearch} value="Search" /> */}
-    </form>
+    <input
+      type="search"
+      onChange={searchHandler}
+      value={searchText}
+      placeholder="Search a user ..."
+    />
+
+    <div className="search-list">
+      {searchList.map((user) => <a key={user.name} onClick={() => userSelect(user)}>{user.name}</a>)}
+    </div>
   </header>
 );
 
 Header.propTypes = {
   searchText: PropType.string.isRequired,
   searchHandler: PropType.func.isRequired,
-  // submitSearch: PropType.func.isRequired,
+  searchList: PropType.array.isRequired,
+  userSelect: PropType.func.isRequired,
 };
 
 export default Header;
