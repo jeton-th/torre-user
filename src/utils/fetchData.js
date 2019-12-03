9c09517aa5165af8ee1287c424c6daa13ad9aec5
Jeton Thaçi <jetonthaci@outlook.com>
2019-12-03 12:57:08 +0100

    add cors-anywhere

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const fetchData = async (url) => {
-  const result = await axios.get(url,
+  const result = await axios.get(`https://cors-anywhere.herokuapp.com/${url}`,
     {
       mode: 'no-cors',
       headers: {
